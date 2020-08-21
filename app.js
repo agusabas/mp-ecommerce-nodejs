72ff058adc7729d4b9793a343ab799d0bcff8486
Felipe Blanco <felipeblan@gmail.com>
2020-08-21 09:34:22 -0500

    Axios, params updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
+const axios = require('axios');
 
 var mercadopago = require('mercadopago');
 
@@ -55,7 +56,31 @@ app.get('/checkout', function (req, res) {
 
 app.post('/procesar_pago', function (req, res) {
   var body = req.body;
+  // var payment = {
+  //   token: body.token,
+  //   installments: body.installments,
+  //   transaction_amount: body.transaction_amount,
+  //   description: body.description,
+  //   payment_method_id: body.payment_method_id,
+  //   payer: {
+  //     email: TEST_USER_COMPRADOR.email,
+  //     identification: {
+  //       number: body.docNumber,
+  //       type: body.docType
+  //     }
+  //   }
+  // };
   res.send(body);
+});
+
+app.get('/test', (req, res) => {
+  const url = `https://api.mercadopago.com/v1/payment_methods?access_token=${TEST_USER_VENDEDOR.accessToken}`;
+  axios.get(url).then(r => {
+    console.log(r);
+    res.send(r.data);
+  }).catch(e => {
+    res.status(400).send(e);
+  });
 });
 
 app.use(express.static('assets'));
